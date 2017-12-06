d1fbb71ad2b5f5ab6dfbf2c1cbe9e7904bdee19d
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-06 14:04:44 +0100

    f

--- Pipes-Theme/sitehead.php
+++ Pipes-Theme/sitehead.php
@@ -2,12 +2,10 @@
 
     <div class="header">
     <div id="logo">
-    	Hej
     </div>
       <nav class="headerMenu">
       		<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
       </nav>
-    	<p class="NameName">hej</p>
     </div>
 
       

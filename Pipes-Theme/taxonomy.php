755467f90bf852d75a4a1853d955b6802c9054fc
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-08 12:20:21 +0100

    c

--- Pipes-Theme/taxonomy.php
+++ Pipes-Theme/taxonomy.php
@@ -1,17 +1,20 @@
 <?php get_header(); ?>
-<div class="background">
-</div>
-<div class="content">
-	<h1> <?php single_cat_title() ?></h1>
-	<div class="listof">
-		<?php if (have_posts()){
-			while (have_posts()) : the_post();?>
-				<ul class='imggrid'>
-					<li>
-						<?php echo "<h2>";the_title();echo "</h2>"; ?>
-					</li>
-				</ul>
-		<?php endwhile; }?>
+<div id='content'>
+	<div class="background">
+		<nav class="kategorimeny">
+      		<?php wp_nav_menu( array( 'theme_location' => 'extra-meny' ) );?>
+      	</nav>
+		<h1> <?php single_cat_title() ?></h1>
+		<div class="listof">
+			<?php if (have_posts()){
+				while (have_posts()) : the_post();?>
+					<ul class='imggrid'>
+						<li>
+							<?php echo "<h2>";the_title();echo "</h2>"; ?>
+						</li>
+					</ul>
+				<?php endwhile; }?>
+		</div>
 	</div>
 </div>
 <style></style>

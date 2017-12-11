ce3b6f936f9d0c0fe11178cb8fd0b9096c01bfe9
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-11 15:22:02 +0100

    d

--- Pipes-Theme/homeheader.php
+++ Pipes-Theme/homeheader.php
@@ -7,88 +7,52 @@
     <link href="https://fonts.googleapis.com/css?family=Contrail+One|Forum|Knewave|Maiden+Orange" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script>
+		$(document).ready( function() { // Add a open class, enable to show or not the navigation 
+			var icon = $("#showmenu");
+			var menu = $("#menu-huvudmeny");
 
-
-			$(document).ready( function() { // Add a open class, enable to show or not the navigation 
-				var icon = $("#showmenu");
-				var menu = $("#menu-huvudmeny");
-
-				icon.click( function(event) {
-					event.preventDefault();
-					menu.toggleClass("open");
-					icon.toggleClass("open");
-				} );
-
+			icon.click( function(event) {
+				event.preventDefault();
+				menu.toggleClass("open");
+				icon.toggleClass("open");
 			} );
-		</script>
+
+		} );
+	</script>
     <?php wp_head()?>
   </head>
 
   <body>
 
-  
-
-
 <header>
     <div class="header2">
-    	<a href="#" id="showmenu"></a>
-    	<div id="menyHome">
-	      <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
+    	<div class="blackoverlay">
+	    	<div class="navigation">
+		    	<a href="#" id="showmenu"></a>
+		    	<div id="menyHome">
+			      <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
+		    	</div>
+		    </div>
+	
+		    <div id="logo2"></div>
+		    <div id="adress">
+		      <?php dynamic_sidebar( 'page-sidebar' ); ?>
+		    </div>
 	    </div>
-	    <div id="logo2"></div>
-	    <div id="adress">
-	      <?php dynamic_sidebar( 'page-sidebar' ); ?>
-	    </div>
-
     </div>
 
 </header>
 
 <style type="text/css">
-#menu-huvudmeny{
-	margin: 0px auto;
-	text-align: center;
-	display: block;
-	float: none;
-	background-color: #151515;
-	padding: 20px 0;
 
-
-}
 
 .header2{
+	overflow: hidden;
 	
 }
-#showmenu {
-			float: right;
-			width: 40px;
-			height:40px;
-			padding: 30px 0;
-			background: url("Images/menu.png") no-repeat center center;
-			background-color: transparent;
-			z-index: 100;
-			margin-right:20px;
-			background-size:80%;
-			 transition: all 0.5s ease-in-out;
-		  -moz-transition: all 0.5s ease-in-out;
-		  -webkit-transition: all 0.5s ease-in-out;
-		  -o-transition: all 0.5s ease-in-out;
-		}
-		#showmenu.open{
-			background-color: black;
-			background: url("Images/menuopen.png") no-repeat center center;
-			background-size:80%;
-			transition: all 0.5s ease-in-out;
-		  -moz-transition: all 0.5s ease-in-out;
-		  -webkit-transition: all 0.5s ease-in-out;
-		  -o-transition: all 0.5s ease-in-out;
-		}
 
-		#menu-huvudmeny {
-			clear: both;
-			display: none;
-		}
-		#menu-huvudmeny.open {
-			display: block;
-		}
+#menyHome{
+	width:100%;
+}
+
 </style>

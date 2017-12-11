bd252f37f259baaf56950d2af0af67e5c41ea2db
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-11 18:15:40 +0100

    d

--- Pipes-Theme/footer.php
+++ Pipes-Theme/footer.php
@@ -1,13 +1,13 @@
 <?php wp_footer(); ?>
 
 <div id="footer">
-	<p class="fName"><?php bloginfo('name') ?></p>
+	
 	<div class="links">
-		<a href="" >
+		<a href="https://www.facebook.com/pages/Pipes-of-Scotland/183397055019185" >
 			<div class="facebook">
 			</div>
 		</a>
-		<a href="" >
+		<a href="https://www.instagram.com/pipesjkpg/" >
 			<div class="instagram">
 			</div>
 		</a>
@@ -16,5 +16,11 @@
 			</div>
 		</a>
 	</div>
+	<p><a href="https://www.google.com/maps/place/BagPipesCorner/@57.7813997,14.1578234,17z/data=!3m1!4b1!4m5!3m4!1s0x465a6de9660dded9:0xdd6e09982076d553!8m2!3d57.7813997!4d14.1600174">Brunnsgatan 13, 553 17 Jönköping</a></p>
+	<p><a href="mailto:jonkoping@pipesofscotland.se">	jonkoping@pipesofscotland.se</a> | <a href="036-30 22 40">036-30 22 40<a></p>
+	<a href="index.php" >
+			<div class="footerlogo">
+			</div>
+		</a>
 </div>
 

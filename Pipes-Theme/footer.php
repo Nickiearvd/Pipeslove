776e7acdeb3df5b25e3f015407894692d561edb2
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-11 17:23:17 +0100

    d

--- Pipes-Theme/footer.php
+++ Pipes-Theme/footer.php
@@ -1,10 +1,21 @@
+<?php wp_footer(); ?>
 
 <div id="footer">
 	<p class="fName"><?php bloginfo('name') ?></p>
 	<div class="links">
-		<a class="facebook" href="" ></a>
-		<a class="instagram" href="" ></a>
-		<a class="where" href="https://www.google.com/maps/place/BagPipesCorner/@57.7813997,14.1578234,17z/data=!3m1!4b1!4m5!3m4!1s0x465a6de9660dded9:0xdd6e09982076d553!8m2!3d57.7813997!4d14.1600174" ></a>
+	blalalal
+		<a href="" >
+			<div class="facebook">
+			</div>
+		</a>
+		<a href="" >
+			<div class="instagram">
+			</div>
+		</a>
+		<a href="https://www.google.com/maps/place/BagPipesCorner/@57.7813997,14.1578234,17z/data=!3m1!4b1!4m5!3m4!1s0x465a6de9660dded9:0xdd6e09982076d553!8m2!3d57.7813997!4d14.1600174">
+			<div class="where">
+			</div>
+		</a>
 	</div>
 </div>
 

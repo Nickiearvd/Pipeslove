ff5da4dd1e3d52dc2535acfe8c0911cfdfbd53fb
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-11 19:20:32 +0100

    d

--- Pipes-Theme/page.php
+++ Pipes-Theme/page.php
@@ -1,30 +1,29 @@
 <?php get_header();?>
 <div id="content">
-<div class="background">
-    <div id="post">
-    	<div id="imageSide">
-            <?php the_post_thumbnail();?>
-        </div>
-    	<div id="textSide">
+    <div class="background">
+        <div class="c">
+            <div id="post">
+            	<div id="imageSide">
+                    <?php the_post_thumbnail();?>
+                </div>
+            	<div id="textSide">
 
-        	<p> 
-                <?php 
-                    if(have_posts()): 
-                        while ( have_posts()) :
-                            the_post(); 
-                            the_content();
-                        endwhile;
-                    endif; 
-                ?>       
-           </p>
-           
-
-
+                	<p> 
+                        <?php 
+                            if(have_posts()): 
+                                while ( have_posts()) :
+                                    the_post(); 
+                                    the_content();
+                                endwhile;
+                            endif; 
+                        ?>       
+                   </p>
+               </div>
+               
+               </div>
+            </div>
        </div>
-       
-       </div>
-   </div>
-</div>
+    </div>
 </div>        
            
     <footer>

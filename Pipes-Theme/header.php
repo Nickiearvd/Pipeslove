1a2c731d4db36569cfed689700141ae654426b38
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-11 15:43:02 +0100

    s

--- Pipes-Theme/header.php
+++ Pipes-Theme/header.php
@@ -5,6 +5,20 @@
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Contrail+One|Forum|Knewave|Maiden+Orange" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+		$(document).ready( function() { // Add a open class, enable to show or not the navigation 
+			var icon = $("#showmenu");
+			var menu = $("#menu-huvudmeny");
+
+			icon.click( function(event) {
+				event.preventDefault();
+				menu.toggleClass("open");
+				icon.toggleClass("open");
+			} );
+
+		} );
+	</script>
     <?php wp_head()?>
   </head>
 
--- Pipes-Theme/sitehead.php
+++ Pipes-Theme/sitehead.php
@@ -1,12 +1,18 @@
 <header>
 
-    <div class="header">
-    <div id="logo">
-    </div>
-      <nav class="headerMenu">
-      		<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
-      </nav>
-    </div>
-
+	<div class="header">
+	    <div class="blackoverlay">
+		    <div class="navigation">
+		    	<div id="logo">
+	   			</div>
+			    <a href="#" id="showmenu"></a>
+			    <nav class="headerMenu">
+				    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
+			    </nav>
+			</div>
+		</div>
+	</div>
+    
       
 </header>
+

2cfc7dcc3c1563f454e9e3fe3bcdb87eab33ffb0
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-12 15:20:12 +0100

    d

--- Pipes-Theme/archive.php
+++ Pipes-Theme/archive.php
@@ -1,15 +1,14 @@
 <?php get_header(); ?>
 
 <div id='content'>
-<div class="background">
-<div id="menytopper"></div>
-<nav class="kategorimeny">
-<h1>Meny</h1>
-			
-      		<?php wp_nav_menu( array( 'theme_location' => 'extra-meny' ) );?>
-      </nav>
-
-</div>
+	<div class="background">
+	<div class="c">
+		<nav class="kategorimeny">
+			<h1>Meny</h1>	
+		    <?php wp_nav_menu( array( 'theme_location' => 'extra-meny' ) );?>
+	    </nav>
+	</div>
+	</div>
 </div>
 	
 <?php get_footer(); ?>
@@ -19,12 +18,12 @@
 			html{
 				position:relative;
 			}
+
+			.c{
+				padding-top: 140px;
+			}
 			.background{
-				background-size: 100%;
-				background-color: #f5f5f5;
-				width:100%;
-				top:0;
-				z-index: 100;
+	
 
 			}
 			#content{
@@ -33,30 +32,18 @@
 			}
 			h1{
 				color: rgba(255, 255, 255, 0.9);
-				margin-top: 80px;
 			}
-			#menytopper{
-				background-color: #151515;
-				height: 70px;
-
-			}
+			
 			.postgrid{
 				z-index: 10;
 
 			}
 			
 			.kategorimeny{
-
-				
-				background: url(https://www.pixeltopic.com/files/2017/12/hjvpqcuputxsntb.jpg) no-repeat;
-				filter: grayscale(50%);
-				background-size: 100%;
 				padding-top:10px;
 				padding-bottom:100px;
 			}
 			.kategorimeny ul {
-				width:90%;
-				max-width: 500px;
 				box-sizing: border-box;
 				padding:0;
 				margin: 30px auto 0 auto;
@@ -67,14 +54,16 @@
 				.kategorimeny ul li{
 					list-style: none;
 					text-align: center;
-					margin: 0 auto 18px auto;
-					height: 35px;
+					margin: 10px auto 10px auto;
+					padding:0px auto;
+					height: 50px;
 					width: 280px;
 					background-color: rgba(255, 255, 255, 0.9);
 					-moz-transition: all 1s;
 					-webkit-transition: all 1s;
 					transition: all 1s;
-				
+					border-bottom: 1px solid;
+					border-color: rgba(0, 0, 0, 0.3);
 				}
 				
 				.kategorimeny ul li:hover, .kategorimeny ul li:first-child:hover, .kategorimeny ul li:last-child:hover{
@@ -83,7 +72,7 @@
 					-moz-transition: all 0.3s;
 					-webkit-transition: all 0.3s;
 					transition: all 0.3s;
-					box-sizing: border-box;
+
 				}
 				.kategorimeny a{
 					text-transform: uppercase;
@@ -91,23 +80,43 @@
 					text-decoration: none;
 					font-weight: 300;
 					color: rgba(0,0,0, 0.8);
-					line-height: 35px;
+					line-height: 50px;
 					font-size: 18px;
 					padding: 0;
 
 				}
 
 				.kategorimeny ul li:last-child{
-					
+					border-bottom: none;
 				}
+
 				.kategorimeny ul li:first-child{
 				
 					
 				}
 
 				.kategorimeny a:hover{
+					font-weight: 500;
+					font-size: 20px;
+					-moz-transition: all 0.5s;
+					-webkit-transition: all 0.5s;
+					transition: all 0.5s;
 					
-					text-decoration: underline;
+				}
+				.menu{
+					background-color: white;
+					width:100%;
+					margin:0;
+					padding:0;
+				}
+				#menu-extrameny{
+					margin:0;
+					width:100%;
+					margin: 40px auto 0 auto;
+
+				}
+
+				@media (min-width: 600px) {
 					
 				}
 			</style>

f50aa9c7e07520c777fdac9323d40ab04b0f78aa
Nickiearvd <arvidssonnickie@gmail.com>
2017-12-12 15:38:01 +0100

    dd

--- Pipes-Theme/archive.php
+++ Pipes-Theme/archive.php
@@ -20,7 +20,9 @@
 			}
 
 			.c{
-				padding-top: 140px;
+				padding-top: 160px;
+				padding-bottom: 140px;
+
 			}
 			.background{
 	
@@ -39,82 +41,7 @@
 
 			}
 			
-			.kategorimeny{
-				padding-top:10px;
-				padding-bottom:100px;
-			}
-			.kategorimeny ul {
-				box-sizing: border-box;
-				padding:0;
-				margin: 30px auto 0 auto;
-				overflow: hidden;
-
-			}
-
-				.kategorimeny ul li{
-					list-style: none;
-					text-align: center;
-					margin: 10px auto 10px auto;
-					padding:0px auto;
-					height: 50px;
-					width: 280px;
-					background-color: rgba(255, 255, 255, 0.9);
-					-moz-transition: all 1s;
-					-webkit-transition: all 1s;
-					transition: all 1s;
-					border-bottom: 1px solid;
-					border-color: rgba(0, 0, 0, 0.3);
-				}
-				
-				.kategorimeny ul li:hover, .kategorimeny ul li:first-child:hover, .kategorimeny ul li:last-child:hover{
-					background: #fff;
-					cursor: pointer;
-					-moz-transition: all 0.3s;
-					-webkit-transition: all 0.3s;
-					transition: all 0.3s;
-
-				}
-				.kategorimeny a{
-					text-transform: uppercase;
-					font-family: 'open sans', helvetica;
-					text-decoration: none;
-					font-weight: 300;
-					color: rgba(0,0,0, 0.8);
-					line-height: 50px;
-					font-size: 18px;
-					padding: 0;
-
-				}
-
-				.kategorimeny ul li:last-child{
-					border-bottom: none;
-				}
-
-				.kategorimeny ul li:first-child{
-				
-					
-				}
-
-				.kategorimeny a:hover{
-					font-weight: 500;
-					font-size: 20px;
-					-moz-transition: all 0.5s;
-					-webkit-transition: all 0.5s;
-					transition: all 0.5s;
-					
-				}
-				.menu{
-					background-color: white;
-					width:100%;
-					margin:0;
-					padding:0;
-				}
-				#menu-extrameny{
-					margin:0;
-					width:100%;
-					margin: 40px auto 0 auto;
-
-				}
+			
 
 				@media (min-width: 600px) {
 					

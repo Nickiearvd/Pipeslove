8b49cba6f02d306c0c86b3a0b099a0b442ee3faf
annadarner <darner.96@hotmail.com>
2017-12-14 14:11:33 +0100

    viewport

--- Pipes-Theme/homeheader.php
+++ Pipes-Theme/homeheader.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Contrail+One|Forum|Knewave|Maiden+Orange" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
